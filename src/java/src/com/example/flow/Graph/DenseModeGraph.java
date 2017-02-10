3c1b8945b6c0bb8d13052efb180ffc3da33f8ebd
Pool_Little <hansu@cisco.com>
2017-02-10 16:56:34 +0800

    Feb 10, 2017

---
+++ src/java/src/com/example/flow/Graph/DenseModeGraph.java
@@ -0,0 +1,126 @@
+package com.example.flow.Graph;
+import java.util.Vector;
+import javax.management.loading.PrivateClassLoader;
+
+import org.omg.PortableServer.ServantActivator;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import java.util.Stack;
+
+public class DenseModeGraph {
+	/**
+	 * 属性
+	 * 节点个数，边个数，是否为有向图，Vector对象啊存储图的邻接矩阵。
+	 */
+	private int nodes;
+	private int edges;
+	private boolean directed;
+	private Vector<Vector> graph = new Vector<Vector>();
+	/**
+	 * 构造函数
+	 * @param nodes		-节点个数
+	 * @param directed 	-是否为有向图
+	 */
+	public DenseModeGraph(int nodes, boolean directed){
+		this.nodes = nodes;
+		this.directed = directed;
+		graph = new Vector<Vector>(nodes);
+		for(int i = 0 ; i < nodes ; i ++){
+			Vector<Integer> v_tmp = new Vector<Integer>();
+			for(int j = 0 ; j < nodes ; j ++){
+				v_tmp.add(0);
+			}
+			graph.add(v_tmp);
+		}
+	}
+	/**
+	 * Gets函数
+	 * 提供节点数与边数的查询
+	 */
+	public int GetNodes(){
+		return this.nodes;
+	}
+	public int GetEdges(){
+		return this.edges;
+	}
+	/**
+	 * 查询函数
+	 * 查询途中是够含有指定边
+	 * @param x -one edge
+	 * @param y	-the other edge
+	 * @return	-true or false
+	 */
+	boolean hasEdge(int x, int y){
+		//这里只考虑无相图，因此只需要判断一个方向的变即可
+		if(graph.get(x).get(y).equals(1))
+			return true;
+		return false;
+	}
+	/**
+	 * addEdge函数
+	 * 针对无向图，需要双向添加。
+	 * @param x	-one edge
+	 * @param y	-the other edge
+	 */
+	public void addEdge(int x, int y){
+		graph.get(x).set(y, 1);
+		if(this.directed == false)
+			graph.get(y).set(x, 1);
+		this.edges ++;
+	}
+	/**
+	 * show函数
+	 * 打印图的邻接矩阵
+	 */
+	public void printGraph(){
+		System.out.print("\t");
+		for(int i = 0 ; i < this.nodes ; i ++){
+			System.out.print(i);
+			System.out.print("\t");
+		}
+		System.out.print("\n");
+		for(int i = 0 ; i < this.nodes ; i ++){
+			System.out.print(i);
+			System.out.print("\t");
+			for(int j = 0 ; j < this.nodes ; j ++){
+				System.out.print(this.graph.get(i).get(j));
+				System.out.print("\t");
+			}
+			System.out.print("\n");
+		}
+	}
+	/**
+	 * adjIterator
+	 * 指定节点的临边迭代器
+	 */
+	public class adjIterator{
+		//指定图象
+		private DenseModeGraph Graph;
+		//目标节点
+		private int node;
+		//临边索引值
+		private int index;
+		//construction
+		public adjIterator(DenseModeGraph graph, int node){
+			this.Graph = graph;
+			this.node = node;
+		}
+		//three main methods
+		public int begin(){
+				index = -1;
+				return next();
+			}
+		public int next(){
+			for(index += 1 ; index < Graph.GetNodes() ; index ++){
+				if(Graph.graph.get(node).get(index).equals(1))
+					return index;
+			}
+			return -1;
+		}
+		public	boolean end(){
+			return index >= Graph.GetNodes();
+		}
+	}
+	
+}
---
+++ src/java/src/com/example/flow/flowRFS.java
@@ -0,0 +1,101 @@
+package com.example.flow;
+
+import com.example.flow.namespaces.*;
+import com.example.flow.Graph.*;
+import com.example.flow.Graph.DenseModeGraph.adjIterator;
+
+import java.util.List;
+import java.util.Properties;
+import com.tailf.conf.*;
+import com.tailf.navu.*;
+import com.tailf.ncs.ns.Ncs;
+import com.tailf.dp.*;
+import com.tailf.dp.annotations.*;
+import com.tailf.dp.proto.*;
+import com.tailf.dp.services.*;
+import com.tailf.ncs.template.Template;
+import com.tailf.ncs.template.TemplateVariables;
+
+
+public class flowRFS {
+
+
+    /**
+     * Create callback method.
+     * This method is called when a service instance committed due to a create
+     * or update event.
+     *
+     * This method returns a opaque as a Properties object that can be null.
+     * If not null it is stored persistently by Ncs.
+     * This object is then delivered as argument to new calls of the create
+     * method for this service (fastmap algorithm).
+     * This way the user can store and later modify persistent data outside
+     * the service model that might be needed.
+     *
+     * @param context - The current ServiceContext object
+     * @param service - The NavuNode references the service node.
+     * @param ncsRoot - This NavuNode references the ncs root.
+     * @param opaque  - Parameter contains a Properties object.
+     *                  This object may be used to transfer
+     *                  additional information between consecutive
+     *                  calls to the create callback.  It is always
+     *                  null in the first call. I.e. when the service
+     *                  is first created.
+     * @return Properties the returning opaque instance
+     * @throws ConfException
+     */
+
+    @ServiceCallback(servicePoint="graphImport-servicepoint",
+        callType=ServiceCBType.CREATE)
+    public Properties graphImport(ServiceContext context,
+             NavuNode service,
+             NavuNode ncsRoot,
+             Properties opaque)
+             throws ConfException {
+    	/**
+    	 * 加载graph对象
+    	 * 设备名的最后两个字符为设备ID，作为graph节点ID
+    	 */
+    	//测试输出当前所有连接设备ID
+    	/*NavuContainer devices = ncsRoot.container("devices");
+    	NavuList device = devices.list("device");
+    	for(NavuContainer device_info : device){
+    		System.out.print(device_info.leaf("name").valueAsString());
+    		System.out.println("\t");
+    	}*/
+    	//测试输出所有yang数据
+    	/*NavuList nodeInfo = service.list("nodeInfo");
+    	for(NavuContainer node_info : nodeInfo){
+    		System.out.print(node_info.leaf("deviceID").valueAsString());
+    		System.out.print(":");
+    		System.out.print(node_info.leaf("deviceName").valueAsString());
+    		System.out.print("\n");
+    	}*/
+    	
+    	int nodeNum = Integer.valueOf(service.leaf("nodeNum").valueAsString());
+    	DenseModeGraph g1 = new DenseModeGraph(nodeNum, false);
+    	for (NavuContainer edge_info : service.list("edge")) {
+    		g1.addEdge(Integer.valueOf(edge_info.leaf("souDevice").valueAsString()), Integer.valueOf(edge_info.leaf("desDevice").valueAsString()));
+		}
+    	g1.printGraph();
+    	adjIterator adj1 = g1.new adjIterator(g1, 0);
+    	for(int i = adj1.begin() ; !adj1.end() ; i = adj1.next()){
+    		System.out.print(i);
+    	}
+    	
+    	
+    	
+    	
+        return opaque;
+    }
+    
+    @ServiceCallback(servicePoint="flowTable-servicepoint",
+		callType=ServiceCBType.CREATE)
+    public Properties flowTable(ServiceContext context,
+    		NavuNode service,
+    		NavuNode ncsRoot,
+    		Properties opaque)
+			throws ConfException{
+    	return opaque;
+    }
+}
