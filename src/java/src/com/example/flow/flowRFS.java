54e67233776f0f075298b43c93f51de4034fe32d
Pool_Little <hansu@cisco.com>
2017-02-22 12:45:28 +0800

    Feb 22, 2017

--- src/java/src/com/example/flow/flowRFS.java
+++ src/java/src/com/example/flow/flowRFS.java
@@ -7,6 +7,9 @@ import com.example.flow.Graph.Flow;
 import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import com.tailf.conf.*;
 import com.tailf.navu.*;
 import com.tailf.ncs.ns.Ncs;
@@ -60,14 +63,23 @@ public class flowRFS {
     	//graphImport part
     	NavuContainer graphImport = service.container("graphImport");
     	NavuList graph = graphImport.list("graph");
+    	/**
+    	 * it is only support to add one graph per service point now
+    	 */
+    	//the next three lines are use for translate between deviceID and deviceName
+    	NodeUtil nu_tmp = new NodeUtil();
+    	NavuContainer ncUtility = new NavuContainer();
+    	DenseModeGraph graphTmp = null;
+    	
     	for(NavuContainer graphInfo : graph){
-    		NodeUtil nu_tmp = new NodeUtil();
+    		ncUtility = graphInfo;
     		int nodeNum = Integer.valueOf(graphInfo.leaf("nodeNum").valueAsString());
-        	DenseModeGraph g1 = new DenseModeGraph(nodeNum, false);
+    		DenseModeGraph g1 = new DenseModeGraph(nodeNum, false);
         	for (NavuContainer edge_info : graphInfo.list("edge")) {
         		g1.addEdge(Integer.valueOf(edge_info.leaf("souDevice").valueAsString()), Integer.valueOf(edge_info.leaf("desDevice").valueAsString()));
     		}
         	g1.printGraph();
+        	graphTmp = g1;
         	Vector<Integer> v1 =  new Vector<Integer>();
         	Path p1 = new Path(g1, 0);
         	v1 = p1.GetPath(5);
@@ -90,7 +102,25 @@ public class flowRFS {
     	NavuContainer flowTable = service.container("flowTable");
     	NavuList flow = flowTable.list("flow");
     	for(NavuContainer flowInfo : flow){
+    		NavuContainer source_node = flowInfo.container("source_node");
+    		NavuLeaf sourceDevice = source_node.leaf("Device");
+    		NavuLeaf sourcePrefix = source_node.leaf("Prefix");
+    		NavuLeaf sourceNetmask = source_node.leaf("Netmask");
     		
+    		NavuContainer des_node = flowInfo.container("des_node");
+    		NavuLeaf desDevice = des_node.leaf("Device");
+    		NavuLeaf desPrefix = des_node.leaf("Prefix");
+    		NavuLeaf desNetmask = des_node.leaf("Netmask");
+    		System.out.print(sourcePrefix.valueAsString());
+    		System.out.print(nu_tmp.Get_nodeID(ncUtility, sourceDevice.valueAsString()));
+    		System.out.print(Integer.valueOf(sourceNetmask.valueAsString()));
+    		System.out.print(desPrefix.valueAsString());
+    		System.out.print(nu_tmp.Get_nodeID(ncUtility, desDevice.valueAsString()));
+    		System.out.print(Integer.valueOf(desNetmask.valueAsString()));
+    		Flow flowTemp = new Flow(sourcePrefix.valueAsString(), nu_tmp.Get_nodeID(ncUtility, sourceDevice.valueAsString()), Integer.valueOf(sourceNetmask.valueAsString()), desPrefix.valueAsString(), nu_tmp.Get_nodeID(ncUtility, desDevice.valueAsString()), Integer.valueOf(desNetmask.valueAsString()));	
+    		if(flowInfo.leaf("pathType").valueAsString().equals("auto")){
+    			Vector<Integer> flowVector = flowTemp.forwardPath(graphTmp);
+    		}
     	}
         return opaque;
     }
